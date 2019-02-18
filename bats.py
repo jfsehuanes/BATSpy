b1dc9d780a11aa45e06503a3be95e85ccf81da62
jfsehuanes <jfsehuanes@gmail.com>
2019-02-18 18:49:23 +0100

    successfully detected calls and extracted a window around them for later analysis. Need to clean-up!

--- bats.py
+++ bats.py
@@ -6,7 +6,7 @@ import matplotlib.pyplot as plt
 
 from thunderfish.dataloader import load_data
 from thunderfish.powerspectrum import spectrogram, decibel
-from thunderfish.eventdetection import detect_peaks, threshold_crossings
+from thunderfish.eventdetection import detect_peaks
 
 from IPython import embed
 
@@ -18,7 +18,7 @@ class Batspy:
         self.file_path = file_path
         self.file_name = file_path.split('/')[-1]
         self.freq_resolution = f_resolution  # in Hz
-        self.overlap_frac = overlap_frac  # Overlap fraction of the NFFT windows
+        self.overlap_frac = overlap_frac  # Overlap fraction of the FFT windows
         self.dynamic_range = dynamic_range  # in dB
         self.pcTape_rec = pcTape_rec  # Was this file recorded by PC-Tape?
         self.multiCH = multiCH  # Does this file was recorded with other channels simultaneously?
@@ -64,7 +64,7 @@ class Batspy:
             self.plot_spectrogram()
         pass
 
-    def plot_spectrogram(self, in_kHz=True, ret_fig_and_ax=False):
+    def plot_spectrogram(self, spec_mat, f_arr, t_arr, in_kHz=True, ret_fig_and_ax=False):
         if not self.spectrogram_computed:
             self.compute_spectrogram()
 
@@ -76,17 +76,15 @@ class Batspy:
         inch_factor = 2.54
         fs = 14
         fig, ax = plt.subplots(figsize=(56. / inch_factor, 30. / inch_factor))
-        im = ax.imshow(self.plt_spec, cmap='jet',
-                       extent=[self.t[0], self.t[-1],
-                               int(self.f[0])/hz_fac, int(self.f[-1])/hz_fac],  # divide by 1000 for kHz
+        im = ax.imshow(spec_mat, cmap='jet',
+                       extent=[t_arr[0], t_arr[-1],
+                               int(f_arr[0])/hz_fac, int(f_arr[-1])/hz_fac],  # divide by 1000 for kHz
                        aspect='auto', origin='lower', alpha=0.7)
-        # ToDo: Impossible to update the colorbar ticks for them to be multiples of 10!!!
-        cb_ticks = np.arange(0, self.dynamic_range + 10, 10)
 
         cb = fig.colorbar(im)
 
         cb.set_label('dB', fontsize=fs)
-        ax.set_ylabel('Frequency [Hz]', fontsize=fs)
+        ax.set_ylabel('Frequency [kHz]', fontsize=fs)
         ax.set_xlabel('Time [sec]', fontsize=fs)
         self.spectrogram_plotted = True
 
@@ -95,11 +93,16 @@ class Batspy:
         else:
             pass
 
-    def detect_calls(self, det_range=(40000, 150000), plot_debug=False, plot_in_spec=False, save_spec_w_calls=False):
+    def detect_calls(self, strict_th=False, det_range=(40000, 150000), plot_debug=False,
+                     plot_in_spec=False, save_spec_w_calls=False):
 
         # Get an average over all frequency channels within detection range
         av_power = np.mean(self.spec_mat[np.logical_and(self.f > det_range[0], self.f < det_range[1])], axis=0)
-        th = np.min(av_power)  # THIS THRESHOLD ROCKS YOUR PANTS! for more detections, increase f_res. 2^7 or 2^8
+
+        if strict_th:  # either search for really good quality calls or else for just a rough detection
+            th = np.percentile(av_power, 99)
+        else:
+            th = np.min(av_power)  # THIS THRESHOLD ROCKS YOUR PANTS! for more detections, increase f_res. 2^7 or 2^8
 
         # Fix cases where th <= 0
         if th <= 0:
@@ -115,7 +118,7 @@ class Batspy:
             # plt.show()
 
         if plot_in_spec:
-            spec_fig, spec_ax = self.plot_spectrogram(ret_fig_and_ax=True)
+            spec_fig, spec_ax = self.plot_spectrogram(self.plt_spec, self.f, self.t, ret_fig_and_ax=True)
             spec_ax.plot(self.t[peaks], np.ones(len(peaks))*80, 'o', ms=20,  # plots the detection at 80kHz
                          color='darkred', alpha=.8, mec='k', mew=3)
             spec_fig.suptitle(self.file_name.split('.')[0])
@@ -165,6 +168,38 @@ if __name__ == '__main__':
         
         bat = Batspy(recording, f_resolution=2**9, overlap_frac=.70, dynamic_range=70)  # 2^7 = 128
         bat.compute_spectrogram()
-        bat.detect_calls(plot_in_spec=True)
-        plt.show()
-        quit()
+        average_power, peaks, _ = bat.detect_calls(strict_th=True, plot_in_spec=False)
+
+        # Goal now is to create small windows for each call
+        # make a time array with the sampling rate
+        time = np.arange(0, len(bat.recording_trace) / bat.sampling_rate, 1/bat.sampling_rate)
+        window_width = 0.010  # in seconds
+        # now follow the call windows
+        call_windows = [bat.recording_trace[np.logical_and(time >= bat.t[e]-window_width/2.,
+                                                           time <= bat.t[e]+window_width/2.)]
+                        for e in peaks]
+        call_specs = [[] for e in np.arange(len(call_windows))]
+
+        for i in np.arange(len(call_windows)):
+            s, f, t = spectrogram(call_windows[i], samplerate=bat.sampling_rate, fresolution=2**11,
+                                  overlap_frac=0.99)
+
+            # set dynamic range
+            single_spec_dyn_range = 90
+            dec_spec = decibel(s)
+            # define maximum; use nanmax, because decibel function may contain NaN values
+            ampl_max = np.nanmax(dec_spec)
+            dec_spec -= ampl_max + 1e-20  # subtract maximum so that the maximum value is set to lim x--> -0
+            dec_spec[dec_spec < -single_spec_dyn_range] = -single_spec_dyn_range
+            call_specs[i] = dec_spec
+            fig, ax = bat.plot_spectrogram(dec_spec, f, t, ret_fig_and_ax=True)
+            import os
+            save_path = '../../data/temp_batspy/' + '/'.join(bat.file_path.split('/')[5:-1]) +\
+                        '/' + bat.file_name.split('.')[0] + '/'
+            if not os.path.exists(save_path):
+                os.makedirs(save_path)
+
+            fig.savefig(save_path + 'fig_' + str(i) + '.pdf')
+            plt.close(fig)
+
+        print('\nDONE!')
