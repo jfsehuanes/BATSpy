9e9589956253f7d7943bc5ff4196a3a1b072fd92
jfsehuanes <jfsehuanes@gmail.com>
2019-05-07 16:05:22 +0200

    now using audioio. It writes a readable wav file. Next step is to lower the amplitude of the signal, set cutoffs at beginning and end and insert further calls.

--- stimgen.py
+++ stimgen.py
@@ -1,14 +1,14 @@
 import numpy as np
-from scipy.io.wavfile import write
+import audioio
 
 from IPython import embed
 
 if __name__ == '__main__':
-    f0 = 1000.  # Start frequency in Hz
-    fe = 200. * 1000.  # End frequency in Hz
+    f0 = 180000.  # Start frequency in Hz
+    fe = 90000.  # End frequency in Hz
     t0 = 0.  # Start time in s
-    te = 1  # End time in s
-    samp_freq = 500. * 1000.  # in Hz
+    te = 0.2  # End time in s
+    samp_freq = 600. * 1000.  # in Hz
     time = np.arange(t0, te, 1./samp_freq)
 
     # Define the slope of the phase function
@@ -22,6 +22,6 @@ if __name__ == '__main__':
 
     # Save the signal
     out_file = 'test_result/stim.wav'
-    write(out_file, int(samp_freq), s)
+    audioio.write_audio(out_file, s, samp_freq, 'wav')
 
     # ToDo: Make a signal 5s long containing multiple 2ms sweeps
