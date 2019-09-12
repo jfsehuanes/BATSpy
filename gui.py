1cc42f6a735081052898b965901db8d7b2826e9f
Juan F. Sehuanes <jfsehuanes@github.com>
2019-09-12 16:56:50 -0500

    gui interacting with backend. need to fit the canvas of the main window!

--- gui.py
+++ gui.py
@@ -1,7 +1,10 @@
 import warnings
 warnings.filterwarnings("ignore", category=RuntimeWarning)
 import sys
-from PyQt5.QtWidgets import QMainWindow, QApplication, QAction, QDesktopWidget, QFileDialog
+from PyQt5.QtWidgets import QMainWindow, QApplication, QAction, QDesktopWidget, QFileDialog, QPushButton, QToolTip
+from PyQt5.QtGui import QFont
+
+from bats import Batspy
 
 from IPython import embed
 
@@ -9,8 +12,10 @@ class MainWindow(QMainWindow):
     
     def __init__(self, verbose=3):
         super().__init__()
-        self.InitFunc()
         self.verbose = verbose
+
+        # Initialize function
+        self.InitFunc()
 
     def open(self):
         openObj = QAction('&Open', self)
@@ -21,10 +26,25 @@ class MainWindow(QMainWindow):
 
     def click_open(self):
         fd = QFileDialog()
-        fname = fd.getOpenFileName(self, 'Select File', '~/', 'Please select .wav files only (*.wav )')[0]
+        self.fname = fd.getOpenFileName(self, 'Select File', '~/', 'Please select .wav files only (*.wav )')[0]
         if self.verbose == 3:
-            print('opening file %s' %fname)
-        self.statusBar().showMessage("%s selected" % fname)
+            print('opening file %s' %self.fname)
+        if len(self.fname) > 0:
+            self.fname_selected = True
+            self.statusBar().showMessage("%s selected" % self.fname)
+
+    def load_singleCH(self):
+
+        if not self.fname_selected:
+            self.click_open()
+
+        bat = Batspy(self.fname, f_resolution=2 ** 9, overlap_frac=.70, dynamic_range=50, pcTape_rec=False)
+        bat.compute_spectrogram()
+        bat.plot_spectrogram(showit=True)
+        # pows, pks = bat.detect_calls(det_range=(80000, 150000), plot_in_spec=True)
+        # embed()
+        # plt.show()
+        # quit()
 
     def quit(self):
         quitObj = QAction('&Quit', self)
@@ -34,6 +54,10 @@ class MainWindow(QMainWindow):
         return quitObj
 
     def InitFunc(self):
+
+        # boolean flow control
+        self.fname_selected = False
+        self.file_loaded = False
 
         # status bar
         self.statusBar().showMessage('Welcome to BATSpy!')
@@ -53,7 +77,7 @@ class MainWindow(QMainWindow):
 
         # Calls Submenu
         calls = menubar.addMenu('&Calls')
-        
+
         # get current screen resolution
         sSize = QDesktopWidget().screenGeometry(-1)
         sWidth = sSize.width()
@@ -63,6 +87,14 @@ class MainWindow(QMainWindow):
         # Establish main window size and title
         self.setGeometry(sWidth/2 - mwLength/2, sHeight/2 - mwLength/2, mwLength, mwLength)
         self.setWindowTitle('BATSpy')
+
+        # Load singleCH button
+        loadSCh = QPushButton('Load Single Channel', self)
+        loadSCh.move(mwLength - mwLength / 2, mwLength - 100)
+        loadSCh.released.connect(self.load_singleCH)
+        # Set tool tip
+        QToolTip.setFont(QFont('Arial', 14))
+        loadSCh.setToolTip('Analyze current file as a <b>single channel<\b>')
 
         self.show()
 
