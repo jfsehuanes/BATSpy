db0898fbcede3e0148d34f83ba8d3e42f928097e
Juan F. Sehuanes <jfsehuanes@github.com>
2019-09-17 09:30:46 -0500

    before drawing new canvas on which to plot stuff

--- gui.py
+++ gui.py
@@ -1,8 +1,13 @@
 import warnings
 warnings.filterwarnings("ignore", category=RuntimeWarning)
 import sys
-from PyQt5.QtWidgets import QMainWindow, QApplication, QAction, QDesktopWidget, QFileDialog, QPushButton, QToolTip
+from PyQt5.QtWidgets import QWidget, QMainWindow, QApplication, QAction, QDesktopWidget, QFileDialog, QPushButton, QToolTip,\
+    QVBoxLayout
 from PyQt5.QtGui import QFont
+from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
+from matplotlib.backends.backend_qt5agg import NavigationToolbar2QT as NavigationToolbar
+
+import matplotlib.pyplot as plt
 
 from bats import Batspy
 
@@ -12,6 +17,9 @@ class MainWindow(QMainWindow):
     
     def __init__(self, verbose=3):
         super().__init__()
+        self._main = QWidget()
+        self.setCentralWidget(self._main)
+
         self.verbose = verbose
 
         # Initialize function
@@ -31,16 +39,28 @@ class MainWindow(QMainWindow):
             print('opening file %s' %self.fname)
         if len(self.fname) > 0:
             self.fname_selected = True
-            self.statusBar().showMessage("%s selected" % self.fname)
+            self.statusBar().showMessage("%s selected" % ('.../' + '/'.join(self.fname.split('/')[-3:])))
 
     def load_singleCH(self):
 
         if not self.fname_selected:
             self.click_open()
 
+        # close previous figure if plotted
+        if self.multiCH_loaded or self.singleCH_loaded:
+            self.fig.clear()
+
         bat = Batspy(self.fname, f_resolution=2 ** 9, overlap_frac=.70, dynamic_range=50, pcTape_rec=False)
         bat.compute_spectrogram()
-        bat.plot_spectrogram(showit=True)
+        self.fig, ax = bat.plot_spectrogram(ret_fig_and_ax=True, showit=False)
+        plt.draw()
+
+        # plot
+        self.plotWidget = FigureCanvas(self.fig)
+        lay = QVBoxLayout(self._main)
+        lay.setContentsMargins(0, 0, 0, 0)
+        lay.addWidget(self.plotWidget)
+
         # pows, pks = bat.detect_calls(det_range=(80000, 150000), plot_in_spec=True)
         # embed()
         # plt.show()
@@ -57,7 +77,9 @@ class MainWindow(QMainWindow):
 
         # boolean flow control
         self.fname_selected = False
-        self.file_loaded = False
+        self.singleCH_loaded = False
+        self.multiCH_loaded = False
+        self.fig = None
 
         # status bar
         self.statusBar().showMessage('Welcome to BATSpy!')
@@ -87,6 +109,8 @@ class MainWindow(QMainWindow):
         # Establish main window size and title
         self.setGeometry(sWidth/2 - mwLength/2, sHeight/2 - mwLength/2, mwLength, mwLength)
         self.setWindowTitle('BATSpy')
+
+        # Draw the main Canvas
 
         # Load singleCH button
         loadSCh = QPushButton('Load Single Channel', self)
