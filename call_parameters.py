9d9256bcab4fd28a233c04fadc32ad68d9a1c20d
Juan F. Sehuanes <jfsehuanes@github.com>
2021-04-28 19:32:03 +0200

    Jan helped hurraaa

--- call_parameters.py
+++ call_parameters.py
@@ -6,8 +6,7 @@ import matplotlib.pyplot as plt
 import matplotlib.mlab as mlab
 from thunderfish.powerspectrum import decibel
 from thunderfish.dataloader import load_data
-from thunderfish.eventdetection import detect_peaks, threshold_crossings, minmax_threshold, hist_threshold,\
-    percentile_threshold
+from thunderfish.eventdetection import threshold_crossings, remove_events
 
 from IPython import embed
 
@@ -67,62 +66,28 @@ def best_channel(rec_ls, calls, window_width=0.010, nfft=2 ** 8, overlap_percent
     return retCalls, retChs
 
 
-def smooth(y, box_pts):
-    box = np.ones(box_pts)/box_pts
-    y_smooth = np.convolve(y, box, mode='same')
-    return y_smooth
+def extract_call_boundaries(hist, meanNoise, dbTh=-10):
+
+    dbHist = decibel(hist, None)
+
+    meanNoisedB = decibel(meanNoise, np.max(hist))
+    if dbTh - meanNoisedB < 5:  # meanNoisedB has to be at least 5 dB below threshold
+        print('+++++++++ noise floor too close to threshold. Ignoring this call +++++++++')
+        return dbHist, dbTh, np.nan, np.nan
+
+    up, down = threshold_crossings(dbHist, dbTh)
+
+    up, down = remove_events(up, down, 2)
+
+    if len(up) == 0 or len(down) == 0:
+        print('+++++++++ No threshold crossings found! +++++++++')
+        return dbHist, dbTh, np.nan, np.nan
+
+    return dbHist, dbTh, up[0]+1, down[-1]  # +1 because threshold_crossings takes the index before th cross
 
 
-def extract_freqs():
-
-    pass
-
-
-def extract_call_boundaries(summed_hist, peakInx, histAxis, threshType=None, convBoxPoints=None, thFac=0.4):
-
-    if convBoxPoints == None:
-        convBoxPoints = int(np.ceil(0.15 * len(summed_hist)))
-    # Create a convolution filter for smoothing out the signal and so come around the Nullstellen-issue
-    smthHist = smooth(summed_hist, convBoxPoints)
-
-    # find the crossings of the smoothed summed histogram
-    if threshType == 'minmax':
-        th = minmax_threshold(smthHist, thresh_fac=thFac)
-    elif threshType == 'hist':
-        th, _ = hist_threshold(smthHist, thresh_fac=thFac)
-    else:
-        raise(TypeError("not a valid threshold type. Please specify between 'minmax' and 'hist'"))
-
-    up, down = threshold_crossings(smthHist, th)
-
-    if th >= np.max(smthHist):
-        print('+++++++++ WARNING!! Call boundaries not detected! +++++++++')
-        return smthHist, th, np.nan, np.nan
-
-    elif len(up) == 0 or len(down) == 0:
-        print('increasing threshold!')
-        extract_call_boundaries(summed_hist, peakInx, histAxis, threshType='minmax', convBoxPoints=convBoxPoints,
-                                thFac=thFac*2)
-
-    # check whether the peak time index lies within the detected time call boundaries
-    peakInTheMiddle = False
-    crossInxUp = np.where(peakInx - up > 0)[0][-1]
-    crossInxDown = np.where(down - peakInx > 0)[0][0]
-
-    if crossInxUp == crossInxDown:
-        peakInTheMiddle = True
-
-    if peakInTheMiddle:
-        return smthHist, th, up[crossInxUp], down[crossInxDown]
-    else:
-        print('+++++++++ WARNING!! Call peak not within call boundaries! +++++++++')
-        return smthHist, th, np.nan, np.nan
-
-
-def call_window(recFile, callT, winWidth=0.030, nfft=2 ** 9, overlap_percent=0.6, plotDebug=False, dynRange=70):
-    # load data
-    dat, sr, u = load_data(recFile)
-    dat = np.hstack(dat)
+def call_window(dat, sr, callT, winWidth=0.030, pkWidth=0.005, nfft=2 ** 7, overlap_percent=0.6, plotDebug=False,
+                dynRange=70):
 
     # define spec window
     time = np.arange(0, len(dat)/sr, 1/sr)
@@ -131,10 +96,8 @@ def call_window(recFile, callT, winWidth=0.030, nfft=2 ** 9, overlap_percent=0.6
     s, f, t = mlab.specgram(dat[windIdx], Fs=sr, NFFT=nfft,
                             noverlap=int(overlap_percent * nfft))  # Compute a high-res spectrogram of the window
 
-    dec_spec = decibel(s)
-
     call_freq_range = (50000, 200000)
-    filtered_spec = dec_spec[np.logical_and(f > call_freq_range[0], f < call_freq_range[1])]
+    filtered_spec = s[np.logical_and(f > call_freq_range[0], f < call_freq_range[1])]
     freqs_of_filtspec = np.linspace(call_freq_range[0], call_freq_range[-1], np.shape(filtered_spec)[0])
 
     # get peak frequency
@@ -149,38 +112,43 @@ def call_window(recFile, callT, winWidth=0.030, nfft=2 ** 9, overlap_percent=0.6
     newT = np.linspace(time[windIdx][0], time[windIdx][-1], len(t))
 
     # call begin and end computed using summed histograms along both the time and frequency axes of the spectrogram
-    narrowSpec = s[np.logical_and(f > call_freq_range[0], f < call_freq_range[1])]
-    # meanNoise = np.mean(narrowSpec[:noiseInds, :])
-    freqHist = np.sum(narrowSpec, axis=1)
-    timeHist = np.sum(narrowSpec, axis=0)
+    narrowSpec = filtered_spec[:, np.logical_and(t >= t[pkfTimeIdx]-pkWidth/2, t <= t[pkfTimeIdx]+pkWidth/2)]
 
-    # ToDo: I think it's better to work with a hist_threshold for the freqs and minmax_threshold for time
-    # smthFreqHist, fBegin, fEnd = extract_call_boundaries(freqHist, pkfFreqIdx, freqs_of_filtspec, threshType='minmax',
-    #                                                      thFac=0.4)
+    narrowT = np.linspace(t[pkfTimeIdx]-pkWidth/2, t[pkfTimeIdx]+pkWidth/2, np.shape(narrowSpec)[1])
 
-    try:
-        smthTimeHist, smthTHistTh, tLeft, tRight = extract_call_boundaries(timeHist, pkfTimeIdx, newT,
-                                                                       threshType='minmax', thFac=0.4)
-    except:
-        embed()
-        exit()
+    meanNoise = np.mean(s[f > 250000, :])
+
+    freqHist = np.max(narrowSpec, axis=1)
+    timeHist = np.max(narrowSpec, axis=0)
+
+    # RECOMPUTE PEAKFREQ INDEX COORDINATES!!
+    # get peak frequency
+    peak_f_idx = np.unravel_index(narrowSpec.argmax(),
+                                  narrowSpec.shape)
+
+    # first start from peak to right
+    pkfFreqIdx = peak_f_idx[0]
+    pkfTimeIdx = peak_f_idx[1]
+
+    dbTimeHist, dbTimeTh, tLeft, tRight = extract_call_boundaries(timeHist, meanNoise)
 
     # now detect the frequency values
-    smthFreqHist, smthFHistTh, fRight, fLeft = extract_call_boundaries(freqHist, pkfFreqIdx, freqs_of_filtspec,
-                                                                       threshType='minmax', thFac=0.4)
-    # embed()
-    # exit()
+    dbFreqHist, dbFreqTh, fRight, fLeft = extract_call_boundaries(freqHist, meanNoise)
+
+    if np.any(np.isnan([fRight, fLeft, tRight, tLeft])):
+        print('+++++++++++++++No boundaries found. inserting nans!!+++++++++++++')
+        return np.nan, np.nan, np.nan, np.nan
 
     fBegin = freqs_of_filtspec[fLeft]
     fEnd = freqs_of_filtspec[fRight]
 
-    tBegin = newT[tLeft]
-    tEnd = newT[tRight]
+    tBegin = narrowT[tLeft]
+    tEnd = narrowT[tRight]
     cDur = tEnd - tBegin
     pkFreq = freqs_of_filtspec[pkfFreqIdx]
 
     # debug plot
-    # ToDo: add the summed histograms to the DeBug plot
+    # ToDo: Plot the mean noise floor in dB!!
     if plotDebug:
         inch_factor = 2.54
         fs = 24
@@ -191,24 +159,20 @@ def call_window(recFile, callT, winWidth=0.030, nfft=2 ** 9, overlap_percent=0.6
         ax1 = fig.add_subplot(gs[1, :-1])
         ax2 = fig.add_subplot(gs[0:-1, -1])
 
-        # define max and min of the spectrogram to be plotted
-        maxSpec = np.max(filtered_spec)
-        minSpec = np.min(filtered_spec - maxSpec)
-
-        im = ax0.imshow(filtered_spec - maxSpec, cmap='jet',
-                        extent=[newT[0], newT[-1],
+        im = ax0.imshow(decibel(narrowSpec, None), cmap='jet',
+                        extent=[narrowT[0], narrowT[-1]+np.diff(narrowT)[0],
                                 int(freqs_of_filtspec[0]) / 1000,
-                                int(freqs_of_filtspec[-1]) / 1000],  # divide by 1000 for kHz
-                        aspect='auto', interpolation='hanning', origin='lower', alpha=0.7, vmin=minSpec - minSpec/4,
+                                int(freqs_of_filtspec[-1]+np.diff(freqs_of_filtspec)[0]) / 1000],
+                        aspect='auto', origin='lower', alpha=0.7, vmin=-dynRange,
                         vmax=0.)
 
-        ax2.plot(smthFreqHist, freqs_of_filtspec/1000, 'k', lw=2)
-        ax2.plot(np.ones(len(freqs_of_filtspec))*smthFHistTh, freqs_of_filtspec/1000, '--b', lw=2)
+        ax2.plot(dbFreqHist, freqs_of_filtspec/1000, 'k', lw=2)
+        ax2.plot(np.ones(len(freqs_of_filtspec))*dbFreqTh, freqs_of_filtspec/1000, '--b', lw=2)
 
         # now plot the detected parameters inside the spectrogram
         ax0.plot([tBegin, tEnd], np.array([fBegin, fEnd]) / 1000,
                  'ow', ms=10, mec='k', mew=2)
-        ax0.plot(newT[peak_f_idx[1]], freqs_of_filtspec[peak_f_idx[0]] / 1000,
+        ax0.plot(narrowT[peak_f_idx[1]], freqs_of_filtspec[peak_f_idx[0]] / 1000,
                  'or', ms=13, mec='k', mew=2)
 
         ax0.set_ylabel('Frequency [kHz]', fontsize=fs + 1)
@@ -219,15 +183,13 @@ def call_window(recFile, callT, winWidth=0.030, nfft=2 ** 9, overlap_percent=0.6
         for c_ax in [ax0, ax1, ax2]:
             c_ax.tick_params(labelsize=fs)
 
-        ax0.set_ylim(int(freqs_of_filtspec[0]) / 1000, int(freqs_of_filtspec[-1]) / 1000)
 
         # Plot the soundwave underneath the spectrogram
-        ax1.plot(newT, smthTimeHist, 'k', lw=2)
-        ax1.plot(newT, np.ones(len(newT)) * smthTHistTh, '--b', lw=2)
+        ax1.plot(narrowT, dbTimeHist, 'k', lw=2)
+        ax1.plot(narrowT, np.ones(len(narrowT)) * dbTimeTh, '--b', lw=2)
 
         # Share the time axis of spectrogram and raw sound trace
         ax1.get_shared_x_axes().join(ax0, ax1)
-        ax0.set_xlim(time[windIdx][0], time[windIdx][-1])
 
         # Remove time xticks of the spectrogram
         ax0.xaxis.set_major_locator(plt.NullLocator())
@@ -241,7 +203,7 @@ if __name__ == '__main__':
     csv = pd.read_csv('tmp/approach.csv')
 
     # loop through all sequences in the csv
-    for seqInd in np.arange(len(csv)):
+    for seqInd in range(csv.shape[1]):
         seqName = csv.iloc[:, seqInd].name
         print("\ncurrent sequence: %s" % seqName)
         seq = np.array(csv.iloc[:, seqInd])
@@ -258,15 +220,25 @@ if __name__ == '__main__':
         freqEnd = np.zeros(len(calls))
         peakFreq = np.zeros(len(calls))
 
-        for enu, callT in enumerate(calls):
-            print("Call %i of %i..." % (enu+1, len(calls)))
-            # compute a high res spectrogram of a defined window length
-            dur, fb, fe, pf = call_window(recNames[bch[enu]], callT, plotDebug=True)
+        enu = 0
+        for channel in set(bch):
+            # load data
+            dat, sr, u = load_data(recNames[channel])
+            dat = np.hstack(dat)
 
-            callDur[enu] = dur
-            freqBeg[enu] = fb
-            freqEnd[enu] = fe
-            peakFreq[enu] = pf
+            for n, callT in enumerate(calls[bch==channel]):
 
-        embed()
-        quit()
+                print('analyzing call %i' %(n+1))
+
+                # compute a high res spectrogram of a defined window length
+
+                dur, fb, fe, pf = call_window(dat, sr, callT, plotDebug=True)
+
+                callDur[enu] = dur
+                freqBeg[enu] = fb
+                freqEnd[enu] = fe
+                peakFreq[enu] = pf
+                enu += 1
+
+    embed()
+    quit()
